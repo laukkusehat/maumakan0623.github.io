9e35c61ff0690f7e3749bd4fb4428e130bcd792f
ADL\Fitri_117820 <fitri.yadi@axiatadigitallabs.com>
2023-07-08 15:08:33 +0700

    [ADD] stus transaksi

--- assets/js/initial-var.js
+++ assets/js/initial-var.js
@@ -1,1 +1,1 @@
-var script_url = "https://script.google.com/macros/s/AKfycbwAilF5JQ3oRN_SX4HNrpPvyRC7H-D_mA1NY4DukjEo5lN8HHJOdTOl6ZTZIPCl3lNj/exec";
+var script_url = "https://script.google.com/macros/s/AKfycbykhZLj0CQeA_xRozXTvh5jDukDuz1fi_I7LI5kF0LkcBA5RtoNznuN14d884XdP9OI/exec";
